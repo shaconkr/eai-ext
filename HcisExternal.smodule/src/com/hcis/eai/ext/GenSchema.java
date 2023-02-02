bff7007fdf1298250e24e68e7aea49b80197f099
최형기 신한DS <cis047@hyundaidept.com>
2023-02-02 13:58:47 +0900

    신용불량(채무불이행) 나이스 자료 송신

--- HcisExternal.smodule/src/com/hcis/eai/ext/GenSchema.java
+++ HcisExternal.smodule/src/com/hcis/eai/ext/GenSchema.java
@@ -1,25 +1,47 @@
 package com.hcis.eai.ext;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 
 import com.hcis.eai.common.HCISHelperAnc;
 
 public class GenSchema {
 	
-    String eimsPath = "D:/HCIS/eai-repo/";
-    String projPath = "D:/HCIS/";
+	String eimsPath = "D:/HCIS/eai-repo/";
+	String projPath = "D:/HCIS/eai-ext/";
 	
-	   @Test
-	    public void prepareOne() throws Exception {
-		   String reqIfId = "JNICISCTIC0040101";
-		   String resIfId = "CISJNICTIC0040102";
-	        String packageName = reqIfId.substring(0,10) + "/" + reqIfId;
-	        String moduleName = reqIfId.substring(0,6) +  "301.module";
-	    	String processName = packageName + "." + reqIfId + "_Sub";
+	String reqIfId = "CISNICUPID0010501";
+	String resIfId = "NICCISUPID0010501";
+    
+	String moduleName = reqIfId.substring(0,6) +  "301.module";
+	
+	String packageName = reqIfId.substring(0,10) + "/" + reqIfId;
+	
+   	String processName = packageName + "." + reqIfId + "_Sub";
+   	
+	   
+	@Test
+	public void buildSchemas() throws Exception {	  
+        HCISHelperAnc helper = new HCISHelperAnc(eimsPath, projPath, moduleName);
+        helper.setProcessName(processName);
+        helper.prepare(reqIfId, resIfId);
+	}
+	   
+	   
+	@Test
+	public void afterOne() throws IOException {
+		String processPath = projPath + moduleName + "/Processes";
+		
+		Path reqIf = Paths.get(processPath + "/" + reqIfId.substring(0,10) + "/" + reqIfId );
+		Files.createDirectories(reqIf);
+		
+		Path repIf = Paths.get(processPath + "/" + resIfId.substring(0,10) + "/" + resIfId );
+		Files.createDirectories(repIf);
 		   
-	        HCISHelperAnc helper = new HCISHelperAnc(eimsPath, projPath, moduleName);
-	        helper.setProcessName(processName);
-	        helper.prepare(reqIfId, resIfId);
-	    }
+	}
 
 }

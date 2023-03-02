45eaa57967544e71ac99e9ac9e374249d84f0242
최형기 신한DS <cis047@hyundaidept.com>
2023-03-02 15:50:22 +0900

    마이데이터 배치 6건 개발완료

---
+++ MYDCIS301.module/src/com/hcis/eai/myd/MYDCISHelper.java
@@ -0,0 +1,27 @@
+package com.hcis.eai.myd;
+
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hcis.eai.common.HCISHelper;
+
+
+public class MYDCISHelper<N> extends HCISHelper<N> {
+	
+    private static final Logger log = LoggerFactory.getLogger(MYDCISHelper.class);	
+	
+    @Override
+    public InputStream getBeanXmlInputStream(String xmlpath) {
+    	log.debug("load xml {}", xmlpath);
+		return getClass().getResourceAsStream(xmlpath);
+    }
+    
+    
+	public byte[] testData() {		
+		String testData = "TEST1234567890".repeat(500);
+		return testData.getBytes();
+	}
+
+}
